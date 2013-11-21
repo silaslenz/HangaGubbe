d3b0adb86747316ec849d84a21641bd5cec2d760
Silas Lenz <silas.lenz@gmail.com>
2013-11-21 21:51:13 +0100

    Buttons and lives logic

--- HangaGubbe/src/main/java/com/rorlez/hangagubbe/MainActivity.java
+++ HangaGubbe/src/main/java/com/rorlez/hangagubbe/MainActivity.java
@@ -1,5 +1,6 @@
 package com.rorlez.hangagubbe;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -14,6 +15,7 @@ public class MainActivity extends Activity {
     public TextView txv_lives;
     public int livesLeft = 5;
     public GridLayout btnGrid;
+    public static final String PREFS_NAME = "MyPreferences";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,64 +28,19 @@ public class MainActivity extends Activity {
 
     }
 
-
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-
-        final Button buttona = (Button) findViewById(R.id.button_a);
-        final Button buttonb = (Button) findViewById(R.id.button_b);
-        final Button buttonc = (Button) findViewById(R.id.button_c);
-        final Button buttond = (Button) findViewById(R.id.button_d);
-        final Button buttone = (Button) findViewById(R.id.button_e);
-        final Button buttonf = (Button) findViewById(R.id.button_f);
-        final Button buttong = (Button) findViewById(R.id.button_g);
-        final Button buttonh = (Button) findViewById(R.id.button_h);
-        final Button buttoni = (Button) findViewById(R.id.button_i);
-        final Button buttonj = (Button) findViewById(R.id.button_j);
-        final Button buttonk = (Button) findViewById(R.id.button_k);
-        final Button buttonl = (Button) findViewById(R.id.button_l);
-        final Button buttonm = (Button) findViewById(R.id.button_m);
-        final Button buttonn = (Button) findViewById(R.id.button_n);
-        final Button buttono = (Button) findViewById(R.id.button_o);
-        final Button buttonp = (Button) findViewById(R.id.button_p);
-        final Button buttonq = (Button) findViewById(R.id.button_q);
-        final Button buttonr = (Button) findViewById(R.id.button_r);
-        final Button buttons = (Button) findViewById(R.id.button_s);
-        final Button buttont = (Button) findViewById(R.id.button_t);
-        final Button buttonu = (Button) findViewById(R.id.button_u);
-        final Button buttonv = (Button) findViewById(R.id.button_v);
-        final Button buttonw = (Button) findViewById(R.id.button_w);
-        final Button buttonx = (Button) findViewById(R.id.button_x);
-        final Button buttony = (Button) findViewById(R.id.button_y);
-        final Button buttonz = (Button) findViewById(R.id.button_z);
-        final Button buttonoa = (Button) findViewById(R.id.button_oa);
-        final Button buttonae = (Button) findViewById(R.id.button_ae);
-        final Button buttonoe = (Button) findViewById(R.id.button_oe);
-        final TextView txv_word = (TextView) findViewById(R.id.txv_word);
-
-        buttona.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                int location = -1;
-                boolean endofword = false;
-                while (endofword == false) {
-                    if (current_word.contains("p")) {
-                        location = current_word.indexOf("a", location+1);
-                        System.out.println(location);
-                        txv_word.setText(Integer.toString(location));
-                    }
-                    if (location<0){
-                        endofword=true;
-                    }
-                    buttona.setEnabled(false);
-                }
-            }
-        });
-        return true;
-    }*/
+    public void startGame(View view) {
+        livesLeft=5;
+        setContentView(R.layout.game_activity);
+        btnGrid = (GridLayout) findViewById(R.id.buttonGrid);
+        txv_word = (TextView) findViewById(R.id.txv_word);
+        txv_lives = (TextView) findViewById(R.id.textView);
+        txv_word.setText(lineCounter(current_word));
+        txv_word.setText(lineCounter(current_word));
+    }
     public void disableSomething(View view) {
         Button b = (Button) view;
+        final SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
         b.setEnabled(false);
         String buttonText = b.getText().toString().toLowerCase();
         int location = -1;
@@ -110,13 +67,25 @@ public class MainActivity extends Activity {
 
                 if (livesLeft ==0 ) {
                     txv_lives.setText("Game Over");
-                    btnGrid.setVisibility(View.GONE);
+                    int losses = settings.getInt("losses", 0);
+                    editor.putInt("losses", (losses+=1));
+                    setContentView(R.layout.game_over);
                 }
             }
             if (location < 0) {
                 endofword = true;
             }
             txv_word.setText(new String(wordArray));
+
+            editor.commit();
+        }
+        if (word.contains("_")){
+
+        }
+        else{
+            int wins = settings.getInt("wins", 0);
+            editor.putInt("wins", (wins+=1));
+            setContentView(R.layout.game_over);
         }
     }
 
